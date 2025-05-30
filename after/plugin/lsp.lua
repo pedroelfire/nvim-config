d8c93b0f3f70d661424337acd82d1d36d91bdaba
Pedroelfire <pedroelfire@gmail.com>
2025-05-29 20:57:09 -0600

    Updated to nvim .11

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,39 +1,60 @@
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+-- Configuración LSP para Neovim 0.11
+local lsp = require('lspconfig')
+local mason = require('mason')
+local mason_lsp = require('mason-lspconfig')
 
-lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
-  vim.keymap.set('n', '<leader>i', vim.lsp.buf.code_action, { noremap = true, silent = true })
-
-  -- Mapear un comando para formatear manualmente
-  vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.format({ async = true })
-  end, opts)
-end)
-
-lsp.ensure_installed({
+-- Configurar Mason
+mason.setup()
+mason_lsp.setup({
+  ensure_installed = {
     'eslint',
     'angularls',
-    'ts_ls',
+    'tsserver',
     'html',
+    'astro',
     'ast_grep'
+  }
 })
 
+-- Configurar los servidores LSP
+mason_lsp.setup_handlers({
+  function(server_name)
+    lsp[server_name].setup({})
+  end,
+})
+
+-- Configuración de autocompletado
 local cmp = require('cmp')
 cmp.setup({
-    mapping = {
-        ['<Tab>'] = nil,
-        ['<S-Tab>'] = nil,
-    },
-})
-
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({select=true}),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = nil,
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
 })
 
-lsp.setup()
+-- Atajos de teclado LSP
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
+    local opts = { buffer = bufnr }
+    vim.keymap.set('n', '<leader>i', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+  end,
+})
--- lua/pedroelfire/packer.lua
+++ lua/pedroelfire/packer.lua
@@ -26,25 +26,18 @@ use({
   },
   after = "nvim-web-devicons"
 })
-use ({
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v1.x',
-	requires = {
-		{'neovim/nvim-lspconfig'},             -- Required
-		{'williamboman/mason.nvim'},           -- Optional
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
-		{'hrsh7th/nvim-cmp'},         -- Required
-		{'hrsh7th/cmp-nvim-lsp'},     -- Required
-		{'hrsh7th/cmp-buffer'},       -- Optional
-		{'hrsh7th/cmp-path'},         -- Optional
-		{'saadparwaiz1/cmp_luasnip'}, -- Optional
-		{'hrsh7th/cmp-nvim-lua'},     -- Optional
-		{'L3MON4D3/LuaSnip'},             -- Required
-		{'hrsh7th/cmp-buffer'},
-		{'hrsh7th/cmp-path'},
-		{'saadparwaiz1/cmp_luasnip'},
-		{'rafamadriz/friendly-snippets'}, -- Optional
-	}
-})
+  use {'williamboman/mason.nvim'}
+  use {'williamboman/mason-lspconfig.nvim'}
+  use {'neovim/nvim-lspconfig'}
+  
+  -- Autocompletado
+  use {'hrsh7th/nvim-cmp'}
+  use {'hrsh7th/cmp-nvim-lsp'}
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/cmp-path'}
+  use {'hrsh7th/cmp-cmdline'}
+  use {'L3MON4D3/LuaSnip'}
+  use {'saadparwaiz1/cmp_luasnip'}
+  use {'rafamadriz/friendly-snippets'}
 end)
 
